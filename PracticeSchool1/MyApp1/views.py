e42e28b8cc3077ea12cc88ffac1a2b9a3ddd6c99
8001688 <8001688@schoolsnet.act.edu.au>
2026-03-12 13:37:18 +1100

    Inputting a form

--- PracticeSchool1/MyApp1/views.py
+++ PracticeSchool1/MyApp1/views.py
@@ -1,8 +1,9 @@
 import http
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime 
-from .models import teacher 
+from .models import teacher
+from .forms import InputForm
 
 
 def index(request):
@@ -10,3 +11,15 @@ def index(request):
     teach = teacher.objects.all()
     return render(request,"MyApp1/index.html",{'content':teach})
 
+def input_view(request):
+    if request.method == "POST":
+        form = InputForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+
+    else:
+        form = InputForm()
+
+    return render(request, "MyApp1/input.html", {"form": form})
